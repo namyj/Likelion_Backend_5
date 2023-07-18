2274b31c9652662fa8a2dd627899bf8616a17e88
namyj <umjiuji61@gmail.com>
2023-07-18 14:11:44 +0900

    :triangular_flag_on_post: Lecture, Instructor 생성

---
+++ relations/src/main/java/com/example/relations/entity/Instructor.java
@@ -0,0 +1,18 @@
+package com.example.relations.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Instructor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+}
---
+++ relations/src/main/java/com/example/relations/entity/Lecture.java
@@ -0,0 +1,21 @@
+package com.example.relations.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Lecture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    // @JoinColumn(name = "instructor") // 외래키 매핑 할때 사용, name 속성에 매핑할 외래 키 이름을 지정
+    private Instructor instructor;
+
+    private String courseName;
+    private String day;
+    private Integer startTime;
+    private Integer endTime;
+}
