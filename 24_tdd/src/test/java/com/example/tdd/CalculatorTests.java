44a0fe5771e44d3ca1d08b2834f5eb33db59ea07
namyj <umjiuji61@gmail.com>
2023-07-20 13:31:44 +0900

    :white_check_mark: multipleTest 통과

--- 24_tdd/src/test/java/com/example/tdd/CalculatorTests.java
+++ 24_tdd/src/test/java/com/example/tdd/CalculatorTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class CalculatorTests {
     @Test
@@ -11,11 +12,35 @@ public class CalculatorTests {
         Calculator calculator = new Calculator();
 
         assertEquals(5, calculator.add(2, 3));
+        assertNotEquals(5, calculator.add(3, 3));
     }
 
+    @Test
+    public void subtractionTest() {
+        Calculator calculator = new Calculator();
+
+        assertEquals(3, calculator.sub(5, 2));
+    }
+
+    @Test
+    public void multipleTest() {
+        Calculator calculator = new Calculator();
+
+        assertEquals(6, calculator.mul(2, 3));
+    }
+
+
     private class Calculator {
-        public int add(int op1, int op2) {
-            return 5;
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+        public int sub(int a, int b) {
+            return a - b;
+        }
+
+        public int mul(int a, int b) {
+            return a * b;
         }
     }
 }
