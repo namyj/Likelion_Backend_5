c9cd65cf999427ff7b18552e844de9a19bfad1ad
namyj <umjiuji61@gmail.com>
2023-07-20 13:37:52 +0900

    :white_check_mark: divisionTest 통과

--- 24_tdd/src/test/java/com/example/tdd/CalculatorTests.java
+++ 24_tdd/src/test/java/com/example/tdd/CalculatorTests.java
@@ -3,10 +3,10 @@ package com.example.tdd;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTests {
+    // 덧셈 기능
     @Test
     public void additionTest() {
         Calculator calculator = new Calculator();
@@ -15,6 +15,7 @@ public class CalculatorTests {
         assertNotEquals(5, calculator.add(3, 3));
     }
 
+    // 뺄셈 기능
     @Test
     public void subtractionTest() {
         Calculator calculator = new Calculator();
@@ -22,11 +23,25 @@ public class CalculatorTests {
         assertEquals(3, calculator.sub(5, 2));
     }
 
+    // 곱셈 기능
     @Test
     public void multipleTest() {
         Calculator calculator = new Calculator();
 
         assertEquals(6, calculator.mul(2, 3));
+    }
+
+    // 나눗셈 기능
+    // 기본은 반환값이 정수
+    // 0으로 나눌 경우, IllegalArgumentException 발생
+    @Test
+    public void divisionTest() {
+        Calculator calculator = new Calculator();
+
+        assertEquals(2, calculator.div(6, 3));
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.div(6, 0);
+        });
     }
 
 
@@ -41,6 +56,11 @@ public class CalculatorTests {
 
         public int mul(int a, int b) {
             return a * b;
+        }
+
+        public int div(int a, int b) {
+            if (b == 0) throw new IllegalArgumentException();
+            return a / b;
         }
     }
 }
